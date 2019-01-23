68a40d3f224fc7d1b28bcfb2c5a084aef9eda62b
YEJI LEE <yagee97@naver.com>
2019-01-23 11:38:50 +0900

    prog_9
    190123

---
+++ second program/prog_9/prog_9_producer.py
@@ -0,0 +1,21 @@
+from time import sleep
+from json import dumps
+from kafka import KafkaProducer
+import random
+
+producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+                         value_serializer=lambda x:
+                         dumps(x).encode('utf-8'))
+
+for e in range(100):
+    if e % 10 != 0:
+        data = {'student_id': random.randrange(0, 5000),
+                'value': random.randrange(0, 1000)
+                }
+    else:
+        data = {'student_id': random.randrange(0, 5000)
+                }
+
+    producer.send('mongotest', value=data)
+    sleep(5)
+    e += 1
